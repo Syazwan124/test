0492c34a821ede3f1949551d09a0fe305d596355
WanzBotz <100239769+Syazwan124@users.noreply.github.com>
2022-05-20 21:34:55 +0800

    Update rules.js

--- plugins/rules.js
+++ plugins/rules.js
@@ -1,18 +1,18 @@
 let fetch = require('node-fetch')
 let { MessageType } = require('@adiwajshing/baileys')
 let handler = async(m, { conn }) => {
-    let teks = `Kebijakan Privasi, Syarat Ketentuan dan Peraturan Mursid BOT
+    let teks = `Kebijakan Privasi, Syarat Ketentuan dan Peraturan X-WanzBotz
 
 Kebijakan Privasi
-1. MursidBot tidak akan merekam data riwayat chat user.
-2. MursidBot tidak akan menyebarkan nomor users.
-3. MursidBot tidak akan menyimpan media yang dikirimkan oleh users.
-4. MursidBot tidak akan menyalah gunakan data data users.
-5. Owner Mursid berhak melihat data riwayat chat users.
-6. Owner Mursid berhak melihat status users.
-7. Owner Mursid dapat melihat riwayat chat, dan media yang dikirimkan users.
+1. X-WanzBotz tidak akan merekam data riwayat chat user.
+2. X-WanzBotz tidak akan menyebarkan nomor users.
+3. X-WanzBotz tidak akan menyimpan media yang dikirimkan oleh users.
+4. X-WanzBotz tidak akan menyalah gunakan data data users.
+5. Owner Wanz berhak melihat data riwayat chat users.
+6. Owner Wanz berhak melihat status users.
+7. Owner Wanz dapat melihat riwayat chat, dan media yang dikirimkan users.
 
-Peraturan MursidBot
+Peraturan X-WanzBotz
 1. Users dilarang menelpon maupun memvideo call nomor bot.
 2. Users dilarang mengirimkan berbagai bug, virtex, dll ke nomor bot.
 3. Users diharap tidak melakukan spam dalam penggunaan bot.
@@ -20,12 +20,12 @@ Peraturan MursidBot
 5. Users diharap untuk tidak menyalah gunakan fitur fitur bot.
 6. Dilarang keras menggunakan fitur bot 18+
 
-Syarat Ketentuan MursidBot
+Syarat Ketentuan X-WanzBotz
 1. Bot akan keluar dari group apabila sudah waktunya keluar.
-2. MursidBot dapat mem-ban users secara sepihak terlepas dari users salah atau tidak.
-3. MursidBot *tidak akan bertanggungjawab atas apapun yang users lakukan terhadap fitur bot.*
-4. MursidBot akan memberlakukan hukuman: block atau ban terhadap users yang melanggar peraturan.
-5. MursidBot bertanggung jawab atas kesalahan fatal dalam programing maupun owner.
+2. X-WanzBotz dapat mem-ban users secara sepihak terlepas dari users salah atau tidak.
+3. X-WanzBotz *tidak akan bertanggungjawab atas apapun yang users lakukan terhadap fitur bot.*
+4. X-WanzBotz akan memberlakukan hukuman: block atau ban terhadap users yang melanggar peraturan.
+5. X-WanzBotz bertanggung jawab atas kesalahan fatal dalam programing maupun owner.
 
 SC : Thanks to
 -Allah SWT
