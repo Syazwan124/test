b7a778dc1d44f90218a6ed329ed3eacb72ac71e7
WanzBotz <100239769+Syazwan124@users.noreply.github.com>
2022-05-20 21:23:22 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,31 +1,27 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(image + 'donasi')).buffer(), `
 ╭──『 𝐃𝐎𝐍𝐀𝐒𝐈 』──⬣
-│⬡ *_Dana_* :
-│⬡ *088233832771*
+│⬡ *_Touch And Go_* :
+│⬡ *195471037*
 │
-│⬡ *_Pulsa_* :
-│⬡ *089612698583*
-│⬡ *088233832771*
-│
-│⬡ *_Saweria_* :
-│⬡ *saweria.co/mursid25*
+│⬡ *_Digi_* :
+│⬡ *0169650449*
 │
 │⬡ *_Website_*
-│⬡ *_https://bit.ly/3LeJCXu_*
-│⬡ *_https://bit.ly/3LmtovB_*
+│⬡ *_Ngak ada_*
+│⬡ *_Ngak ada_*
 │
 │⬡ *_Owner Bot_* :
-│⬡ *wa.me/6288233832771*
+│⬡ *wa.me/60169650449*
 │
 │⬡「 _*Grup Bot 1*_ 」
-│   https://chat.whatsapp.com/LPFQ2X1cnihB0fb8F8cZau
+│   https://chat.whatsapp.com
 │
 │⬡「 _*Grup Bot 2*_ 」
-│   https://chat.whatsapp.com/HjRHck1G3WRHOx97fJkdMN
+│   https://chat.whatsapp.com
 │
 │⬡「 _*Grup Bot 3*_ 」
-│   https://chat.whatsapp.com/HcGcIB09sIvKrGytO8yfFn
+│   https://chat.whatsapp.com
 ╰──────────────⬣
 `.trim(), footer, 'Owner', '.owner')
 handler.help = ['donasi']
